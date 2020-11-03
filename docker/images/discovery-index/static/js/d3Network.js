1d80871d4396adf7fa15632a87e36319a58baa9d
Shawn O'Neil <oneilsh@gmail.com>
2020-11-03 14:21:05 -0500

    working on d3

--- docker/images/discovery-index/static/js/d3Network.js
+++ docker/images/discovery-index/static/js/d3Network.js
@@ -3,17 +3,20 @@ d3.selectAll("p").style("color", function() {
   console.log("checking")
   return "hsl(" + Math.random() * 360 + ",100%,50%)";
 });*/
-var svg = d3.select('#graphSvg')
+
 var width = $('#graphSvg').width()
 var height = $('#graphSvg').height()
+// select svg and center coordinates on 0, 0 in middle of drawing area
+var svg = d3.select('#graphSvg')
+  .attr("viewBox", [-width / 2, -height / 2, width, height])
 
 //var nodes = [{id: "0", name: "Shawn"}, {id: "1", name: "Brandon"}, {id: "2", name: "Julie"}, {id: "3", name: "Melissa"}]
 //var links = [{source: "0", target: "1", value: 2}, {source: "1", target: "2", value: 4}]
 var nodes = [
-             {id: "0", metadata: {id: "0", labels: ["PrimaryProfile"]}, data: {primaryId: "oneils"}}, 
-             {id: "1", metadata: {id: "1", labels: ["GithubProfile"]}, data: {username: "oneilsh"}}, 
-             {id: "2", metadata: {id: "2", labels: ["PrimaryProfile"]}, data: {}}, 
-             {id: "3", metadata: {id: "3", labels: ["OrcidProfile"]}, data: {primaryId: "0000-0020-0020-0020", firstName: "James", lastName: "Smith"}} 
+             {metadata: {id: "0", labels: ["PrimaryProfile"]}, data: {primaryId: "oneils"}}, 
+             {metadata: {id: "1", labels: ["GithubProfile"]}, data: {username: "oneilsh"}}, 
+             {metadata: {id: "2", labels: ["PrimaryProfile"]}, data: {}}, 
+             {metadata: {id: "3", labels: ["OrcidProfile"]}, data: {primaryId: "0000-0020-0020-0020", firstName: "James", lastName: "Smith"}} 
             ]
 var edges = [
              {metadata: {id: "7", type: "HAS_SECONDARY_PROFILE", data: {ofType: "GitHub"}}, source: "0", target: "1"},
@@ -22,17 +25,66 @@ var edges = [
              {metadata: {id: "10", type: "LINKS_TO", data: {}}, source: "1", target: "3"}
             ]
 
+var current_id = 100
 function addNode() {
-  nodes.push({id: "9", metadata: {id: "9"}, name: "Anne"})
-  edges.push({source: "0", target: "9"})
+  current_id = current_id + 1
+  nodes.push({id: current_id.toString(), metadata: {id: current_id.toString()}, name: "node_" + current_id.toString()})
+  if(current_id > 103) {
+    // why does this not work?
+    edges.push({source: current_id.toString(), target: (current_id - (current_id % 3 + 1)).toString()})
+  }
+
+  // age the nodes
+  nodes = nodes.map(d => {
+    d.age = (d.age || 0) + 1; 
+    if(d.age < 10) {
+      return d
+    } else {
+      delNeighborhood(d)
+    }
+  }).filter(n => n)   // undefined will be filtered as evaulating falsy
+  
+  //nodes = nodes.filter(n => {})
+  
+  // get the simulation to initialize edges with node references sourced from the ID strings given
+  simulation.nodes(nodes)
   simulation.force('link').links(edges)
+  
+  simulation.alpha(0.25).restart() 
 }
 
+// returns an array of [nodes, edges] connected to the given node
+function getNeighborhood(d) {
+  var links = edges.filter(e => e.source == d || e.target == d)
+  var neighbors = links.map(function(e) {
+     if(e.source != d) {
+       return e.source
+     } else {
+       return e.target
+     }    
+  })
+  
+  return [neighbors, links]
+}
+
+// removes d and it's neighbors and links from nodes and edges
+function delNeighborhood(d) {
+  var nhood = getNeighborhood(d)
+  var delNeighbors = nhood[0]
+  var delLinks = nhood[1]
+  edges = edges.filter(function(e) {
+    return delLinks.indexOf(e) == -1
+  })
+  /*nodes = nodes.filter(function(n) {
+    return delNeighbors.indexOf(n) == -1
+  })*/
+  nodes = nodes.filter(n => n != d)
+}
 
 var simulation = d3.forceSimulation(nodes)
-  .force("link", d3.forceLink(edges).id(d => d.id)) 
+  .force("link", d3.forceLink(edges).id(d => d.metadata.id)) 
   .force("charge", d3.forceManyBody().strength(-100))
-  .force("center", d3.forceCenter(width / 2, height / 2))
+  .force("center", d3.forceCenter(0, 0))
   .force("collide", d3.forceCollide().radius(10))
   //.force("charge", d3.forceManyBody().strength(-20).distanceMin(40))
   .on("tick", ticked)
@@ -40,7 +92,7 @@ var simulation = d3.forceSimulation(nodes)
 
 function updateVis(nodes, edges) {
   simulation.nodes(nodes)
-  simulation.force('link', d3.forceLink(edges).id(d => d.id))
+  simulation.force('link', d3.forceLink(edges).id(d => d.metadata.id))
 
 
   var l = d3.select('svg')
@@ -51,10 +103,12 @@ function updateVis(nodes, edges) {
     .join(enter => enter.append('line'))
     .attr('stroke', '#555555')
     .attr('stroke-width', 2)
+//    .attr('opacity', d => {Math.min(ageOpacity(d.source), ageOpacity(d.target))})
     .attr("x1", d => d.source.x)
     .attr("y1", d => d.source.y)
     .attr("x2", d => d.target.x)
-    .attr("y2", d => d.target.y);
+    .attr("y2", d => d.target.y)
+    .on('click', function(event, d) {edges.splice(edges.indexOf(d), 1)})
 
   var u = d3.select('svg')
     .selectAll('circle')
@@ -62,16 +116,29 @@ function updateVis(nodes, edges) {
 
   u
    .join(enter => enter.append('circle')) 
-   .attr("fill", "red")
+   .attr("fill", ageFill)
    .attr("stroke", "white")
    .attr("stroke-width", 2)
    .attr("r", 10)
    .attr('cx', d => d.x)
    .attr('cy', d => d.y)
    .call(drag(simulation))
+   //.on('click', function(event, d) {if(!event.defaultPrevented) console.log("check"); nodes.splice(nodes.indexOf(d), 1)})
+   //.on('click', function(event, d) {delNeighborhood(d)})
+   .raise()
+
+  u
+   .join(enter => enter.append('text'))
+   //.append('text')
+   .text(n => n.metadata.id)
    .raise()
 }
 
+function ageFill(d) {
+  var myColor = d3.scaleLinear().domain([0, 1]).range(["white", "teal"])
+  var point = 1 - Math.min((d.age || 0)/10, 1)
+  return myColor(point)
+}
 
 function ticked() {
   updateVis(nodes, edges)
