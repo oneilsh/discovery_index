f0b0eababfedd02e8483faf323e8f9465333df7e
Shawn O'Neil <oneilsh@gmail.com>
2021-02-15 21:31:38 -0800

    simplelogger test

--- docker/images/discovery-index/lib/logger.js
+++ docker/images/discovery-index/lib/logger.js
@@ -1,8 +1,15 @@
+const simplelogger = require('simple-node-logger').createSimpleLogger('project.log');
 
 exports.stdOutLogger = function(req, res, next) {
   console.log("== Request received:")
   console.log("  - Method: ", req.method)
   console.log("  - URL: ", req.url)
 
+  next()
+}
+
+
+exports.requestLogger = function(req, res, next) {
+  simplelogger.info(req)
   next()
 }
